3040d0f16f01c797ef2816ca9ae1749d9c166471
Anas Chebili <anaschebili@gmail.com>
2024-12-28 15:23:31 +0100

    added tanstack router devtools to project

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,15 +1,25 @@
-import * as React from 'react'
-import { Outlet, createRootRoute } from '@tanstack/react-router'
+import * as React from "react";
+import { Outlet, createRootRoute } from "@tanstack/react-router";
 
 export const Route = createRootRoute({
   component: RootComponent,
-})
+});
 
 function RootComponent() {
   return (
     <React.Fragment>
       <div>Hello "__root"!</div>
       <Outlet />
+      <TanStackRouterDevTools />
     </React.Fragment>
-  )
+  );
 }
+
+const TanStackRouterDevTools =
+  process.env.NODE_ENV === "production"
+    ? () => null
+    : React.lazy(() =>
+        import("@tanstack/router-devtools").then((res) => ({
+          default: res.TanStackRouterDevtools,
+        }))
+      );
