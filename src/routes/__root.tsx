a5c3f52f9b72147086ea5852c22b29c584712dc8
Anas Chebili <anaschebili@gmail.com>
2024-12-28 20:45:35 +0100

    intitial set up for tanstack query

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,17 +1,24 @@
 import * as React from "react";
 import { Outlet, createRootRoute } from "@tanstack/react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export const Route = createRootRoute({
   component: RootComponent,
 });
 
+const queryClient = new QueryClient();
+
 function RootComponent() {
   return (
-    <React.Fragment>
-      <div>Hello "__root"!</div>
-      <Outlet />
-      <TanStackRouterDevTools />
-    </React.Fragment>
+    <QueryClientProvider client={queryClient}>
+      <React.Fragment>
+        <div>Hello "__root"!</div>
+        <Outlet />
+        <TanStackRouterDevTools />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </React.Fragment>
+    </QueryClientProvider>
   );
 }
 
