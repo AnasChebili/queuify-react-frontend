14b6a22646288792314590e6f8adc190091ae59b
Anas Chebili <anaschebili@gmail.com>
2025-01-15 18:22:02 +0100

    implemented UI for adding task feature

---
+++ src/components/task-add.tsx
@@ -0,0 +1,20 @@
+import { Plus } from "lucide-react";
+import { Dispatch, SetStateAction } from "react";
+
+export const TaskAdd = ({
+  onClick,
+}: {
+  onClick: Dispatch<SetStateAction<boolean>>;
+}) => {
+  return (
+    <section className=" cursor-pointer w-[700px] h-[164px] border-dashed border-white rounded-lg border-2 flex justify-center items-center">
+      <div
+        onClick={() => onClick(true)}
+        className="flex flex-col items-center justify-center p-4 transition hover:-translate-y-[1px] hover:bg-opacity-10 items hover:bg-gray-500 rounded-xl"
+      >
+        <Plus className="w-8 h-8" />
+        <h1 className="text-xl">Add Task</h1>
+      </div>
+    </section>
+  );
+};
