b4e858e522d570bad8e0c22e38ba2ac67184e568
Anas Chebili <anaschebili@gmail.com>
2025-01-15 16:54:51 +0100

    created task Update mutation hook; finished implemented the task edit feature, implemented mapping for status values

--- src/components/task-dialog.tsx
+++ src/components/task-dialog.tsx
@@ -2,6 +2,7 @@ import { Dispatch, SetStateAction } from "react";
 import { Dialog, DialogContent } from "./ui/dialog";
 import { TaskForm } from "./task-form";
 import { ResponseTaskSchema } from "@/schemas/task-schema";
+import { DialogDescription, DialogTitle } from "@radix-ui/react-dialog";
 
 type Task = Zod.infer<typeof ResponseTaskSchema>;
 
@@ -17,7 +18,15 @@ export const TaskDialog = ({
   return (
     <Dialog open={open} onOpenChange={() => setOpen((prev) => !prev)}>
       <DialogContent className="bg-[#BCCCDC]">
-        <TaskForm task={task} />
+        <DialogTitle>
+          <header className="flex flex-col gap-1 ">
+            <h1 className="text-lg text-black">Edit Task</h1>
+            <DialogDescription className="text-xs text-gray-600">
+              Modify and edit the task's information
+            </DialogDescription>
+          </header>
+        </DialogTitle>
+        <TaskForm task={task} setOpen={setOpen} />
       </DialogContent>
     </Dialog>
   );
