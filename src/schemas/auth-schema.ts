1d3408d9dac1374f8ce991c433baf728e2b28f5f
Anas Chebili <anaschebili@gmail.com>
2025-01-15 21:06:09 +0100

    added auth schema file with all the necessary schemas for authorization

---
+++ src/schemas/auth-schema.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+
+export const UserRequestSchema = z.object({
+  email: z.string().email(),
+  password: z.string(),
+});
+
+export const UserResponseSchema = z.object({
+  email: z.string(),
+  id: z.string(),
+  createdAt: z.date(),
+  passwordHash: z.string().nullable(),
+  sub: z.string().nullable(),
+  provider: z.string().nullable(),
+});
