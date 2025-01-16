334354c85964b99993a54b72ac282891cc1bb3e7
Anas Chebili <anaschebili@gmail.com>
2025-01-16 14:39:55 +0100

    added logIn function in auth service api to interact with the login API endpoint

--- src/api/api-service/auth/auth-api-service.ts
+++ src/api/api-service/auth/auth-api-service.ts
@@ -1,9 +1,19 @@
 import { api } from "@/api/axios";
 import { ENDPOINTS } from "@/api/endpoints";
+import { UserRequestSchema } from "@/schemas/auth-schema";
 
 export class AuthApiService {
   static async googleLogIn(): Promise<string> {
     const { data } = await api.get(ENDPOINTS.GOOGLE);
+    return data.data;
+  }
+
+  static async logIn({
+    credentials,
+  }: {
+    credentials: Zod.infer<typeof UserRequestSchema>;
+  }): Promise<string> {
+    const { data } = await api.post(`${ENDPOINTS.LOGIN}`, credentials);
     return data.data;
   }
 }
