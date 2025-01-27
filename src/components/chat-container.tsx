f9cee4401bb9840199f43a0a8aaf94f9ec8ca5b7
Anas Chebili <anaschebili@gmail.com>
2025-01-27 19:26:50 +0100

    created jobs api service

--- src/components/chat-container.tsx
+++ src/components/chat-container.tsx
@@ -3,12 +3,15 @@ import { ChatMessageList } from "./chat-message-list";
 import { useWebSocketContext } from "@/hooks/use-websocket";
 import { ChatMessage } from "@/reducers/chat-websocket";
 
+
+
 export const ChatContainer = () => {
   return (
     <section className=" gap-5 text-black rounded-3xl h-[500px] w-[500px] bg-[#BCCCDC] p-3 flex flex-col justify-end ">
       <ChatMessageList />
       <ChatBar />
     </section>
+    
   );
 };
 
