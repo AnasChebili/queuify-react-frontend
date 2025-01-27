6441b84898e00ef25f4d999e933cf5ece864dd63
Anas Chebili <anaschebili@gmail.com>
2025-01-27 20:05:10 +0100

    initialized implementation of jobs interface container

---
+++ src/components/jobs-container.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import { Button } from "./ui/button";
+
+export const JobsContainer = () => {
+  const [isMonitorPage, setIsMonitorPage] = useState(false);
+  return (
+    <section className="w-[500px] p-5">
+      <section className="w-full bg-gray-500 rounded-lg">
+        <Button>Schedule Jobs</Button>
+        <Button>Monitor Jobs</Button>
+      </section>
+    </section>
+  );
+};
