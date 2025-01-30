54216499e15a59f56cee85c147b1c709b6bff597
Anas Chebili <anaschebili@gmail.com>
2025-01-30 21:23:04 +0100

    added tabs to schedule/monitor UI

--- src/components/jobs-container.tsx
+++ src/components/jobs-container.tsx
@@ -2,20 +2,27 @@ import { useState } from "react";
 import { Button } from "./ui/button";
 import { MonitorView } from "./monitor-view";
 import { ScheduleView } from "./schedule-view";
-
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "./ui/tabs";
 export const JobsContainer = () => {
   const [isMonitorView, setIsMonitorView] = useState(true);
   return (
     <section className="w-[500px] p-5 flex flex-col gap-3">
-      <section className="flex w-full bg-gray-500 rounded-lg">
-        <Button className="basis-1/2" onClick={() => setIsMonitorView(false)}>
-          Schedule Jobs
-        </Button>
-        <Button className="basis-1/2" onClick={() => setIsMonitorView(true)}>
-          Monitor Jobs
-        </Button>
-      </section>
-      {isMonitorView ? <MonitorView /> : <ScheduleView />}
+      <Tabs defaultValue="schedule">
+        <TabsList className="w-full">
+          <TabsTrigger className="basis-1/2" value="schedule">
+            Schedule Jobs
+          </TabsTrigger>
+          <TabsTrigger className="basis-1/2" value="monitor">
+            Monitor Jobs
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent value="schedule">
+          <ScheduleView />
+        </TabsContent>
+        <TabsContent value="monitor">
+          <MonitorView />
+        </TabsContent>
+      </Tabs>
     </section>
   );
 };
