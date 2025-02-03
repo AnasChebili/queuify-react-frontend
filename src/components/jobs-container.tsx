a22d2d185e1171d69624ba0535fd8aafb34a752b
Anas Chebili <anaschebili@gmail.com>
2025-02-03 22:26:56 +0100

    switched from state managed tab switching to radix based

--- src/components/jobs-container.tsx
+++ src/components/jobs-container.tsx
@@ -1,10 +1,7 @@
-import { useState } from "react";
-import { Button } from "./ui/button";
 import { MonitorView } from "./monitor-view";
 import { ScheduleView } from "./schedule-view";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "./ui/tabs";
 export const JobsContainer = () => {
-  const [isMonitorView, setIsMonitorView] = useState(true);
   return (
     <section className="w-[500px] p-5 flex flex-col gap-3">
       <Tabs defaultValue="schedule">
