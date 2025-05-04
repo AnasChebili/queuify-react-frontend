21d094f9ae8ae008f1aa11a362c54844a2b69419
Anas Chebili <anaschebili@gmail.com>
2025-05-04 12:01:10 +0200

    changed environment accessing metodology to better comply with vite standards

--- src/components/chat-layout.tsx
+++ src/components/chat-layout.tsx
@@ -4,7 +4,7 @@ import { WebSocketProvider } from "./websocket-provider";
 export const ChatLayout = () => {
   return (
     <WebSocketProvider
-      url={`${process.env.VITE_BACKEND_WEBSOCKET}/websocket/chat`}
+      url={`${import.meta.env.VITE_BACKEND_WEBSOCKET}/websocket/chat`}
     >
       <ChatContainer></ChatContainer>
     </WebSocketProvider>
